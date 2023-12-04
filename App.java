8a1c417984dc3367991eb3c34e7f9567ec806dac
Kishore <skishorekaarthik@gmail.com>
2023-12-04 18:05:51 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,61 +1,78 @@
-import java.util.*;
-import java.lang.Math;
-import java.text.DecimalFormat;
-public class App{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BMICalculator extends JFrame {
+
+    private JTextField heightField, weightField;
+    private JLabel resultLabel;
+
+    public BMICalculator() {
+        setTitle("BMI Calculator");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        initUI();
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void initUI() {
+        setLayout(new BorderLayout());
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(3, 2));
+
+        inputPanel.add(new JLabel("Height (in cm)"));
+        heightField = new JTextField();
+        inputPanel.add(heightField);
+
+        inputPanel.add(new JLabel("Weight (in kg)"));
+        weightField = new JTextField();
+        inputPanel.add(weightField);
+
+        JButton calculateButton = new JButton("Calculate");
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculateBMI();
+            }
+        });
+        inputPanel.add(calculateButton);
+
+        add(inputPanel, BorderLayout.NORTH);
+
+        resultLabel = new JLabel();
+        add(resultLabel, BorderLayout.CENTER);
+    }
+
+    private void calculateBMI() {
+        String heightStr = heightField.getText();
+        String weightStr = weightField.getText();
+
+        if (heightStr.isEmpty() || !heightStr.matches("\\d+")) {
+            resultLabel.setText("Provide a valid Height!");
+        } else if (weightStr.isEmpty() || !weightStr.matches("\\d+")) {
+            resultLabel.setText("Provide a valid Weight!");
+        } else {
+            double height = Double.parseDouble(heightStr);
+            double weight = Double.parseDouble(weightStr);
+
+            double bmi = (weight / ((height * height) / 10000));
+
+            if (bmi < 18.6) {
+                resultLabel.setText("Under Weight: " + String.format("%.2f", bmi));
+            } else if (bmi >= 18.6 && bmi < 24.9) {
+                resultLabel.setText("Normal: " + String.format("%.2f", bmi));
+            } else {
+                resultLabel.setText("Over Weight: " + String.format("%.2f", bmi));
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Select a given option");
-        System.out.println("1.To check BMI in inches and lbs");
-        System.out.println("2.To check BMI in cm and kilograms");
-        int option = sc.nextInt();
-        if(option == 1){
-            System.out.println("Enter your height in inches: ");
-            double height = sc.nextDouble();
-            System.out.println("Enter your weight in lbs: ");
-            double weight = sc.nextDouble();
-            double bodyMass = weight / Math.pow(height, 2) *703;
-            DecimalFormat df = new DecimalFormat("0.0");
-            if (bodyMass < 18.5) {
-           System.out.println("You are Underweight.");
-        }
-        else if (bodyMass < 25) {
-            System.out.println("You are Normal.");
-        }
-        else if (bodyMass < 30) {
-            System.out.println("You are Overweight.");
-        }
-        else {
-           System.out.println("You are Obese.");
-        }
-
-            System.out.println("Your BMI is "+df.format(bodyMass));
-        }
-        else if(option == 2){
-            System.out.println("Enter your height in cm: ");
-            double heightInCm = sc.nextDouble();
-            System.out.println("Enter your weight in kilograms: ");
-            double weightInKg = sc.nextDouble();
-            double bodyMass = weightInKg / Math.pow(heightInCm/100, 2) ;
-            DecimalFormat df = new DecimalFormat("0.0");
-
-            System.out.println("Your BMI is "+df.format(bodyMass));
-            if (bodyMass < 18.5) {
-           System.out.println("You are Underweight.");
-        }
-        else if (bodyMass < 25) {
-            System.out.println("You are Normal.");
-        }
-        else if (bodyMass < 30) {
-            System.out.println("You are Overweight.");
-        }
-        else {
-           System.out.println("You are Obese.");
-        }
-        }
-        else{
-            System.out.println("ERROR! SELECT A VALID OPTION");
-        }
-        sc.close();
-       
+        SwingUtilities.invokeLater(() -> new BMICalculator());
     }
 }
